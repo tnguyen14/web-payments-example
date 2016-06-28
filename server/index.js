77b8a750136b9994f8d9e32e6d910d915f84dbba
Tri Nguyen <tri@tridnguyen.com>
2016-06-28 21:15:45 +0000

    add tls options

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
+require('dotenv-safe').load();
 var request = require('request');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-require('dotenv-safe').load();
+var fs = require('fs');
+var path = require('path');
+var certFilePath = path.resolve(__dirname, '../merchant_id.cer');
+var keyFilePath = path.resolve(__dirname, '../applepay.key');
 
 app.use(bodyParser.json());
 app.post('/merchant-validate', function (req, res) {
 	if (!req.body.validationURL) {
 		return res.status(400).send('Missing validation URL.');
 	}
-	request.post(req.body.validationURL, {
-		merchantId: process.env.MERCHANT_ID,
-		merchantName: process.env.MERCHANT_NAME,
-		merchantDomain: process.env.MERCHANT_DOMAIN
+	request.post({
+		url: req.body.validationURL,
+		json: true,
+		body: {
+			merchantId: process.env.MERCHANT_ID,
+			merchantName: process.env.MERCHANT_NAME,
+			merchantDomain: process.env.MERCHANT_DOMAIN
+		},
+		cert: fs.readFileSync(certFilePath),
+		key: fs.readFileSync(keyFilePath)
 	}, function (err, resp, body) {
 		if (err) {
 			console.error(err);
